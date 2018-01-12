55b37fd1dc8870e7ece49ad35d29ce1944c7791a
poonnanun <poonnanun1@gmail.com>
2018-01-12 14:48:28 +0700

    Complete GameConsole

--- src/GameConsole.java
+++ src/GameConsole.java
@@ -8,17 +8,21 @@ public class GameConsole {
 	/** play the game. */
 	public int play(NumberGame game) {
 		Scanner console = new Scanner(System.in);
-		
+		int over = 0;
 		// describe the game
 		System.out.println( game.toString() );
-		
-		// This is just an example.
-		System.out.println( game.getMessage() );
-		System.out.print("Your answer? ");
-		int guess = console.nextInt();
-		boolean correct = game.guess(guess);
-		System.out.println( game.getMessage() );
-		return guess;
+		while(over == 0){
+			System.out.println( game.getMessage() );
+			System.out.print("Your answer: ");
+			int guess = console.nextInt();
+			boolean correct = game.guess(guess);
+			System.out.println( game.getMessage() );
+			if(correct == true) {
+				over = 1;
+				return guess;
+			}
+		}
+		return 0;
 	}
 	
 }
